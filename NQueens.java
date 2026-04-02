40a3a59f9280778a47859ac3b776317f1a1c1520
sujitha-kotyada <24pa1a05c9@vishnu.edu.in>
2026-04-02 17:53:09 +0530

    Update NQueens.java

--- NQueens.java
+++ NQueens.java
@@ -1,34 +1,47 @@
 //java program for N Queens Problem
-import java.util.*;
+import java.util.Scanner;
+
 public class NQueens {
-static int[] x;
-public static boolean place(int k, int i) {
-      for (int j = 1; j < k; j++)
-              if (x[j] == i || Math.abs(x[j] - i) == Math.abs(j - k))
-                     return false;
-                    return true;
-                    }
-  public static void nqueens(int k, int n) {
-             if (k > n) {
-                for (int i = 1; i <= n; i++)
-                        System.out.print(x[i] + " ");
-                        System.out.println();
-                            
-             }
-          else{
-              for (int i = 1; i <= n; i++){
-                  if (place(k, i)) 
-                      x [k] = i;
-              	nqueens(k + 1, n);
-                             
-              }
-          }
-   }
-   public static void main(String[]  args) {
-           Scanner sc= new  Scanner(System.in);
-           System.out.print("Enter number of queens: ");
-          int n = sc.nextInt();
-           x = new int[n + 1];
-           nqueens(1, n);
+    static int[] x;
+
+    public static boolean place(int k, int i) {
+        for (int j = 1; j < k; j++) {
+            if (x[j] == i || Math.abs(x[j] - i) == Math.abs(j - k))
+                return false;
+        }
+        return true;
+    }
+
+    public static void nqueen(int k, int n) {
+        for (int i = 1; i <= n; i++) {
+            if (place(k, i)) {
+                x[k] = i;
+                if (k == n)
+                    write(n);
+                else
+                    nqueen(k + 1, n);
+            }
+        }
+    }
+
+    public static void write(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (x[i] == j)
+                    System.out.print("Q ");
+                else
+                    System.out.print("_ ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of queens to place:");
+        int n = sc.nextInt();
+        x = new int[n + 1];
+        nqueen(1, n);
     }
 }
